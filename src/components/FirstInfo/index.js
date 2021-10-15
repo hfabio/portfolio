efeab1cb97610cb32167fe8e63c0d13666db531e
Helton Fábio <heltonfabio@outlook.com>
2021-10-15 02:42:49 -0300

    Update index.js

--- src/components/FirstInfo/index.js
+++ src/components/FirstInfo/index.js
@@ -8,7 +8,7 @@ export default function FirstInfo() {
         <div className='firstInfo'>
             <ul>
                 <li className='first'>Helton Fábio</li>
-                <li className='second'>Developer | Bioinformatician 24 years old, Salvador</li>
+                <li className='second'>Developer | Bioinformatician 26 years old, Salvador</li>
                 <li className='third'><span className='-first'>PT-BR</span> | ENG</li>
             </ul>
             <img className='img-fluid w-100 banner' src={ImgBanner} alt="Helton Fábio" />
